fc17606c001007b2197dadb78043ffeae4209435
Renato Valentim <48966099+RenatoValentim@users.noreply.github.com>
2024-07-07 21:57:08 -0300

    feat(binarysearch): return the index when searched value in array and greater than the guess

--- internal/studies/searches/binarysearch_test.go
+++ internal/studies/searches/binarysearch_test.go
@@ -46,6 +46,12 @@ func TestBinarySearch(t *testing.T) {
 			targetValue: 2,
 			expected:    1,
 		},
+		{
+			name:        "Should return the index for the searched value when the value is in the array but greater than the guess",
+			input:       []int{0, 2, 3, 4, 5},
+			targetValue: 4,
+			expected:    3,
+		},
 	}
 
 	for _, tc := range testCases {
