f76df372a3349e8f707018ddb8dd16fcb8798bf2
Renato Valentim <48966099+RenatoValentim@users.noreply.github.com>
2024-08-26 10:41:20 -0300

    feat(quicksort): should sort the array values

--- internal/studies/sort/quicksort.go
+++ internal/studies/sort/quicksort.go
@@ -1,8 +1,40 @@
 package sort
 
+import (
+	"math/rand"
+	"time"
+)
+
 func QuickSort(arr []int) []int {
 	if len(arr) == 0 {
 		return []int{}
 	}
-	return []int{}
+
+	if len(arr) == 1 {
+		return arr
+	}
+
+	rand.NewSource(time.Now().UnixNano())
+	randomIndex := rand.Intn(len(arr))
+
+	pivot := arr[randomIndex]
+	left := []int{}
+	right := []int{}
+	center := []int{}
+
+	for _, value := range arr {
+		if value < pivot {
+			left = append(left, value)
+		}
+
+		if value == pivot {
+			center = append(center, value)
+		}
+
+		if value > pivot {
+			right = append(right, value)
+		}
+	}
+
+	return append(append(QuickSort(left), center...), QuickSort(right)...)
 }
